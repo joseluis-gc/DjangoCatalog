7af80f0c3849b2480e49c23bf397caf86a38797b
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-12-30 16:52:53 -0800

    Customized Admin Panel on Cars App

--- cars/admin.py
+++ cars/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from cars.models import Car
 
-# Register your models here.
 
-admin.site.register(Car)
+# Register your models here.
+class CarAdmin(admin.ModelAdmin):
+
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius:10px;">'.format(object.car_photo.url))
+
+    thumbnail.short_description = 'Car Image'
+
+    list_display = ('id', 'thumbnail', 'car_title', 'model', 'year', 'color', 'body_style', 'fuel_type', 'city', 'is_featured')
+    list_display_links = ('id', 'thumbnail', 'car_title',)
+    list_editable = ('is_featured',)
+    search_fields = ('id', 'car_title', 'city', 'model', 'body_style', 'fuel_type',)
+    list_filter = ('city', 'model',)
+
+
+admin.site.register(Car, CarAdmin)
--- cars/models.py
+++ cars/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-
+from ckeditor.fields import RichTextField
 from django.db import models
+from multiselectfield import MultiSelectField
 
 
 # Create your models here.
@@ -36,13 +37,13 @@ class Car(models.Model):
     year = models.IntegerField('year', choices=year_choice)
     condition = models.CharField(max_length=255)
     price = models.IntegerField()
-    description = models.TextField(max_length=500)
+    description = RichTextField()
     car_photo = models.ImageField(upload_to='photos/%Y/%m/%d')
     car_photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
     car_photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
     car_photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
     car_photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
-    features = models.CharField(choices=features_choices, max_length=255)
+    features = MultiSelectField(choices=features_choices, max_length=255)
     body_style = models.CharField(max_length=255)
     engine = models.CharField(max_length=255)
     transmission = models.CharField(max_length=255)
@@ -56,3 +57,7 @@ class Car(models.Model):
     no_of_owners = models.CharField(max_length=255)
     is_featured = models.BooleanField(default=False)
     created_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.car_title
+
